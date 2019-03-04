182bf931ed1c1271aaf1e1512821254c565c87bf
sergeyvashchenko96 <vashchenkosergey96@gmail.com>
2019-03-04 21:16:21 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,29 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppComponent} from './app.component';
 import {LoginComponent} from './login/login.component';
 import {MatFormFieldModule, MatInputModule} from '@angular/material';
+import {TaskComponent} from './task/task.component';
+import {HeaderComponent} from './header/header.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 const appRoutes: Routes = [
-  {path: '', component: LoginComponent}
+  {path: '', component: LoginComponent},
+  {path: 'task', component: TaskComponent}
+
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    TaskComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
     MatInputModule,
+    MatSidenavModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
