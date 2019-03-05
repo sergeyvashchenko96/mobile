d0c5b3b9d0f6c7c7027a2994ff3ca22070076562
sergeyvashchenko96 <vashchenkosergey96@gmail.com>
2019-03-05 11:05:22 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
-import {Router} from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private router: Router) {
-    this.router.events.subscribe((event: Event) => {
-
-    });
+  constructor() {
   }
 
   title = 'app';
