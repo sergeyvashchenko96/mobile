d9eb1d4d94ae04d16f3083f74351d2d6a4102ea3
sergeyvashchenko96 <vashchenkosergey96@gmail.com>
2019-03-05 10:27:49 +0200

    initial commit

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -6,7 +6,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./task.component.scss']
 })
 export class TaskComponent implements OnInit {
-
+  panelOpenState = true;
   constructor() {
   }
 
